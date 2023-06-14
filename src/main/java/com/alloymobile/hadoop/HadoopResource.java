d745110307859536d0f3f89a9a2d573bd1fb25ae
Tapas Mandal <alloymobileinc@gmail.com>
2023-06-14 17:43:56 -0400

    Added the json dependency

--- src/main/java/com/alloymobile/hadoop/HadoopResource.java
+++ src/main/java/com/alloymobile/hadoop/HadoopResource.java
@@ -1,12 +1,20 @@
 package com.alloymobile.hadoop;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RestController
 public class HadoopResource {
 
     @GetMapping(value= "/hadoop")
-    public String helloWorld(){
-        return "Hello";
+    public String helloWorld(@RequestParam String name){
+
+
+        return "Hello "+name;
+
     }
+
 }
